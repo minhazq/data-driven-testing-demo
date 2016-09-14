14ff66b84e13c582b9da5c1bc82d0a9b125ae5fa
unknown <minhazq@gmail.com>
2016-09-13 20:21:44 -0400

    added code

--- src/test/java/mq/data_driven_testing_demo/Script01.java
+++ src/test/java/mq/data_driven_testing_demo/Script01.java
@@ -1,5 +1,12 @@
 package mq.data_driven_testing_demo;
 
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
 public class Script01 {
@@ -7,6 +14,17 @@ public class Script01 {
 	
 	@Test
 	public void test_method01(){
+		try {
+			Workbook workbook = new XSSFWorkbook("src//test//resources//data.xlsx");
+			Sheet sheet = workbook.getSheet("data");
+			Row row = sheet.getRow(0);
+			Cell cell = row.getCell(0);
+			System.out.println(cell.getStringCellValue());
+			workbook.close();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
